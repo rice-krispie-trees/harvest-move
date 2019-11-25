6f8a975fa1eef48a4991dd5973024a77c11a257b
Jordan Contract <jordan59@optonline.net>
2019-11-25 15:10:56 -0500

    starting with geofirestore functionality

--- ViroSample/firebase/firebase.js
+++ ViroSample/firebase/firebase.js
@@ -1,71 +1,118 @@
-import * as firebase from "firebase"
-import "firebase/firestore"
+import * as firebase from "firebase";
+import "firebase/firestore";
+import {
+  GeoCollectionReference,
+  GeoFirestore,
+  GeoQuery,
+  GeoQuerySnapshot
+} from "geofirestore";
 
 export class FirebaseWrapper {
-	constructor() {
-		this.initialized = false
-		this._firebaseInstance = null
-		this._firebaseWrapperInstance = null
-		this._firestore = null
-	}
+  constructor() {
+    this.initialized = false;
+    this._firebaseInstance = null;
+    this._firebaseWrapperInstance = null;
+    this._firestore = null;
+  }
 
-	Initialize(config) {
-		if (!this.initialized) {
-			this._firebaseInstance = firebase.initializeApp(config)
-			this._firestore = firebase.firestore()
-			//   this._analytics = firebase.analytics();
-			this.initialized = true
-		} else {
-			console.log("already initialized")
-		}
-	}
+  Initialize(config) {
+    if (!this.initialized) {
+      this._firebaseInstance = firebase.initializeApp(config);
+      this._firestore = firebase.firestore();
+      this.initialized = true;
+    } else {
+      console.log("already initialized");
+    }
+  }
 
-	static GetInstance() {
-		if (null == this._firebaseWrapperInstance) {
-			this._firebaseWrapperInstance = new FirebaseWrapper()
-		}
-		return this._firebaseWrapperInstance
-	}
+  static GetInstance() {
+    if (null == this._firebaseWrapperInstance) {
+      this._firebaseWrapperInstance = new FirebaseWrapper();
+    }
+    return this._firebaseWrapperInstance;
+  }
 
-	async CreateNewDocument(collectionPath, doc) {
-		try {
-			const ref = this._firestore.collection(collectionPath).doc()
-			const timestamp = firebase.firestore.Timestamp.now().toDate()
-			return await ref.set({ ...doc, createdAt: timestamp, id: ref.id })
-		} catch (error) {
-			console.log("something went wrong", error)
-		}
-	}
+  async CreateNewDocument(collectionPath, doc) {
+    try {
+      const ref = this._firestore.collection(collectionPath).doc();
+      const timestamp = firebase.firestore.Timestamp.now().toDate();
+      return await ref.set({ ...doc, createdAt: timestamp, id: ref.id });
+    } catch (error) {
+      console.log("something went wrong", error);
+    }
+  }
 
-	async createUser(collectionPath, doc) {
-		try {
-			const ref = this._firestore.collection(collectionPath).doc(doc.user.uid)
-			return await ref.set({
-				email: doc.user.email,
-				profile_picture: doc.additionalUserInfo.profile.picture,
-				first_name: doc.additionalUserInfo.profile.given_name,
-				last_name: doc.additionalUserInfo.profile.last_name,
-				events: []
-			})
-		} catch (error) {
-			console.log("create user failed", error)
-		}
-	}
+  async createUser(collectionPath, doc) {
+    try {
+      const ref = this._firestore.collection(collectionPath).doc(doc.user.uid);
+      return await ref.set({
+        email: doc.user.email,
+        profile_picture: doc.additionalUserInfo.profile.picture,
+        first_name: doc.additionalUserInfo.profile.given_name,
+        last_name: doc.additionalUserInfo.profile.last_name,
+        events: []
+      });
+    } catch (error) {
+      console.log("create user failed", error);
+    }
+  }
 
-	async SetupCollectionListener(collectionPath, callback) {
-		try {
-			await this._firestore
-				.collection(collectionPath)
-				.orderBy("createdAt", "desc")
-				.onSnapshot(querySnapshot => {
-					let container = []
-					querySnapshot.forEach(doc => {
-						container.push(doc.data())
-					})
-					return callback(container)
-				})
-		} catch (error) {
-			console.log("you didnt listen", error)
-		}
-	}
+  async createPlot(collectionPath, name, plotLatitude, plotLongitude) {
+    try {
+      const geofirestore = new GeoFirestore(this._firestore);
+      const geocollection = new GeoCollectionReference(
+        geofirestore.collection(collectionPath)
+      );
+      const location = new firebase.firestore.GeoPoint(
+        plotLatitude,
+        plotLongitude
+      );
+      return await geocollection.add({ location, name });
+    } catch (error) {
+      console.log("create plot failed", error);
+    }
+  }
+
+  async getNearbyPlots(collectionPath, userLatitude, userLongitude, callback) {
+    try {
+      const geofirestore = new GeoFirestore(this._firestore);
+      const geocollection = new GeoCollectionReference(
+        geofirestore.collection(collectionPath)
+      );
+      const location = new firebase.firestore.GeoPoint(
+        userLatitude,
+        userLongitude
+      );
+      const query = new GeoQuery(
+        geocollection.near({ center: location, radius: 0.1 })
+      );
+      await query.get().then((snapshot = new GeoQuerySnapshot()) => {
+        console.log(snapshot.docs);
+        let container = [];
+        snapshot.forEach(doc => {
+          container.push(doc.data());
+        });
+        return callback(container);
+      });
+    } catch (error) {
+      console.log("get nearby plots failed", error);
+    }
+  }
+
+  async SetupCollectionListener(collectionPath, callback) {
+    try {
+      await this._firestore
+        .collection(collectionPath)
+        .orderBy("createdAt", "desc")
+        .onSnapshot(querySnapshot => {
+          let container = [];
+          querySnapshot.forEach(doc => {
+            container.push(doc.data());
+          });
+          return callback(container);
+        });
+    } catch (error) {
+      console.log("you didnt listen", error);
+    }
+  }
 }
--- ViroSample/js/CropMap.js
+++ ViroSample/js/CropMap.js
@@ -1,67 +1,96 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 // import { ExpoConfigView } from '@expo/samples';
-import { StyleSheet, View, Button } from "react-native"
-import MapView, { Marker } from "react-native-maps"
+import { StyleSheet, View, Button } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { FirebaseWrapper } from "../firebase/firebase";
 // import { createStackNavigator, createBottomTabNavigator, navigate } from 'react-navigation';
 
 export default class CropMap extends Component {
-	// return <ExpoConfigView />; // from default template
+  // return <ExpoConfigView />; // from default template
 
-	constructor(props) {
-		super(props)
-		this.state = {
-			latitude: 40.7050758,
-			longitude: -74.0091604,
-			error: 0
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {
+        latitude: 40.7050758,
+        longitude: -74.0091604,
+        error: 0
+      },
+      plots: []
+    };
+    // this.renderPlots = this.renderPlots.bind(this)
+  }
 
-	componentDidMount() {
-		navigator.geolocation.getCurrentPosition(
-			position => {
-				this.setState({
-					latitude: position.coords.latitude,
-					longitude: position.coords.longitude,
-					error: null
-				})
-			},
-			error => this.setState({ error: error.message }),
-			{ enableHighAccuracy: true, timeout: 2000, maximumAge: 1000 }
-		)
-	}
+  async componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          location: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            error: null
+          }
+        });
+      },
+      error => this.setState({ location: { error: error.message } }),
+      { enableHighAccuracy: true, timeout: 2000, maximumAge: 1000 }
+    );
+    await FirebaseWrapper.GetInstance().getNearbyPlots(
+      "Plots",
+      this.state.location.latitude,
+      this.state.location.longitude,
+      plots => this.setState({ plots })
+    );
+  }
 
-	render() {
-		return (
-			<MapView
-				style={styles.map}
-				region={{
-					latitude: this.state.latitude,
-					longitude: this.state.longitude,
-					latitudeDelta: 0.017,
-					longitudeDelta: 0.019
-				}}
-			>
-				<Marker
-					coordinate={this.state}
-					pinColor="blue"
-					onPress={() => this.props.navigation.navigate("Home")}
-				/>
-			</MapView>
-		)
-	}
+  //   async renderPlots() {
+  //     await FirebaseWrapper.GetInstance().getNearbyPlots(
+  //       "Plots",
+  //       this.state.location.latitude,
+  //       this.state.location.longitude,
+  //       plots => this.setState({ plots })
+  //     );
+  //   }
+
+  render() {
+    return (
+		<View>
+			{this.state.plots && this.state.plots.map(plot => {
+				return(
+					<Text>{plot.location}</Text>
+				)
+			})}
+		</View>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: this.state.location.latitude,
+          longitude: this.state.location.longitude,
+          latitudeDelta: 0.017,
+          longitudeDelta: 0.019
+        }}
+      >
+        <Marker
+          coordinate={this.state.location}
+          pinColor="blue"
+          onPress={() => this.props.navigation.navigate("Home")}
+        />
+      </MapView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		...StyleSheet.absoluteFillObject,
-		height: 400,
-		width: 400,
-		justifyContent: "flex-end",
-		alignItems: "center"
-	},
-	map: {
-		...StyleSheet.absoluteFillObject
-	}
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
