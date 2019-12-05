4efb887378e9520d03b221c70fef00b4b78c80c2
Jordan Contract <jordan59@optonline.net>
2019-12-05 18:06:32 -0500

    login is online

--- ViroSample/functions/index.js
+++ ViroSample/functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +6,58 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+const db = admin.firestore();
+
+// const createProfile = (userRecord, context) => {
+//   const { email, uid } = userRecord;
+
+//   return db
+//     .collection("Users")
+//     .doc(uid)
+//     .set({
+//       email,
+//       uid,
+//       name,
+//       plots,
+//       seeds: { corn: 3, potato: 3, cabbage: 3, strawberry: 3, wheat: 3 },
+//       crops: { corn: 0, potato: 0, cabbage: 0, strawberry: 0, wheat: 0 },
+//       hoe: { level: 1 },
+//       wateringCan: { level: 1 },
+//       kolions: 100
+//     })
+//     .catch(console.error);
+// };
+
+// module.exports = {
+//   authOnCreate: functions.auth.user().onCreate(createProfile)
+// };
+
+exports.addUserToFirestore = functions.auth.user().onCreate(async user => {
+  const userRef = db.collection("users").doc(user.uid);
+  return await userRef.set({
+    email: user.email,
+    id: user.uid,
+    seeds: {
+      corn: 3,
+      potato: 3,
+      cabbage: 3,
+      strawberry: 3,
+      wheat: 3
+    },
+    crops: {
+      corn: 0,
+      potato: 0,
+      cabbage: 0,
+      strawberry: 0,
+      wheat: 0
+    },
+    hoe: { level: 1 },
+    wateringCan: { level: 1 },
+    kolions: 100
+  });
+});
--- ViroSample/js/Login.js
+++ ViroSample/js/Login.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 import { connect } from "react-redux";
 import { Actions } from "react-native-router-flux";
-import { loginUser } from "../store/redux/auth";
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input } from 'react-native-elements';
+import { loginUser, logoutUser, signupUser } from "../store/redux/auth";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Input } from "react-native-elements";
 
 class Login extends Component {
   constructor(props) {
@@ -15,7 +15,9 @@ class Login extends Component {
     };
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+    this.handleSignup = this.handleSignup.bind(this);
   }
 
   handleEmailChange = ({ target }) => {
@@ -26,41 +28,72 @@ class Login extends Component {
     this.setState({ password: target.value });
   };
 
-  handleSubmit = () => {
+  handleLogin = () => {
     const { dispatch } = this.props;
     const { email, password } = this.state;
     dispatch(loginUser(email, password));
+    this.setState({ email: "", password: "" });
+  };
+
+  handleLogout = () => {
+    const { dispatch } = this.props;
+    dispatch(logoutUser());
+  };
+
+  handleSignup = () => {
+    const { dispatch } = this.props;
+    const { email, password } = this.state;
+    dispatch(signupUser(email, password));
+    this.setState({ email: "", password: "" });
   };
 
   render() {
-    const { loginError, isAuthenticated } = this.props;
+    const {
+      loginError,
+      logoutError,
+      signupError,
+      isAuthenticated
+    } = this.props;
     if (isAuthenticated) {
       return (
         <View>
           <Button title="Start Farming" onPress={() => Actions.home()} />
+          <Button title="Logout" onPress={() => this.handleLogout()} />
+          {logoutError && (
+            <Text>Couldn't Logout at this time, please try again</Text>
+          )}
         </View>
       );
     } else {
       return (
         <View>
-            <TextInput
-              onChange={this.handleEmailChange}
-              onChangeText={text => this.setState({ email: text })}
-              placeholder="email"
-              value={this.state.email}
-            />
-            <TextInput
-              onChange={this.handlePasswordChange}
-              onChangeText={text => this.setState({ password: text })}
-              placeholder="password"
-              value={this.state.password}
-            />
-            {loginError && <Text>Incorrect email or password</Text>}
-            <Button
-              style={styles.button}
-              title="Sign In"
-              onPress={() => this.handleSubmit()}
-            />
+          <TextInput
+            onChange={this.handleEmailChange}
+            onChangeText={text => this.setState({ email: text })}
+            placeholder="email"
+            value={this.state.email}
+          />
+          <TextInput
+            onChange={this.handlePasswordChange}
+            onChangeText={text => this.setState({ password: text })}
+            placeholder="password"
+            value={this.state.password}
+            secureTextEntry={true}
+          />
+          {loginError && <Text>Incorrect email or password</Text>}
+          {signupError && (
+            <Text>Couldn't create user at this time, please try again</Text>
+          )}
+          <Button
+            style={styles.button}
+            title="Login"
+            onPress={() => this.handleLogin()}
+          />
+          <Button
+            style={styles.button}
+            title="Signup"
+            onPress={() => this.handleSignup()}
+          />
         </View>
       );
     }
@@ -76,8 +109,12 @@ const styles = StyleSheet.create({
 
 const mapStateToProps = state => {
   return {
+    isSigningUp: state.auth.isSigningUp,
     isLoggingIn: state.auth.isLoggingIn,
+    isLoggingOut: state.auth.isLoggingOut,
+    signupError: state.auth.signupError,
     loginError: state.auth.loginError,
+    logoutError: state.auth.logoutError,
     isAuthenticated: state.auth.isAuthenticated
   };
 };
