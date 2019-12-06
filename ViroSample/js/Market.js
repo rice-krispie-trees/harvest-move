7934c37c1bca2af54393f0440dfb8317b28449e4
Tayla Monet <taymjenk@gmail.com>
2019-12-05 19:35:28 -0500

    functional seed modal

--- ViroSample/js/Market.js
+++ ViroSample/js/Market.js
@@ -1,63 +1,81 @@
-import React, { Component } from "react";
-import { StyleSheet, View, Text, ScrollView } from "react-native";
-import { Card, ListItem, Button, Icon } from "react-native-elements";
+import React, { Component } from "react"
+import { StyleSheet, View, Text, ScrollView } from "react-native"
+import { Card, ListItem, Button, Icon } from "react-native-elements"
 
-const Market = () => {
-  return (
-    <View>
-      <ScrollView>
-      <Card title="SELL NOW" image={require("./res/sell_crops.png")}>
-          <Button
-            // icon={<Icon name="code" color="#ffffff" />}
-            buttonStyle={{
-              borderRadius: 0,
-              marginLeft: 0,
-              marginRight: 0,
-              marginBottom: 0
-            }}
-            title="SELECT CROPS TO SELL"
-          />
-        </Card>
-        <Card title="SEEDS" image={require("./res/seed_pack.png")}>
-          <Button
-            // icon={<Icon name="code" color="#ffffff" />}
-            buttonStyle={{
-              borderRadius: 0,
-              marginLeft: 0,
-              marginRight: 0,
-              marginBottom: 0
-            }}
-            title="BUY NOW"
-          />
-        </Card>
-        <Card title="TOOLS" image={require("./res/hoe.png")}>
-          <Button
-            // icon={<Icon name="code" color="#ffffff" />}
-            buttonStyle={{
-              borderRadius: 0,
-              marginLeft: 0,
-              marginRight: 0,
-              marginBottom: 0
-            }}
-            title="BUY NOW"
-          />
-        </Card>
-        <Card title="PROTECTION" image={require("./res/umbrella.png")}>
-          <Button
-            // icon={<Icon name="code" color="#ffffff" />}
-            buttonStyle={{
-              borderRadius: 0,
-              marginLeft: 0,
-              marginRight: 0,
-              marginBottom: 0
-            }}
-            title="BUY NOW"
-          />
-        </Card>
-      </ScrollView>
-    </View>
-  );
-};
+import SeedsModal from "./SeedsModal"
+
+class Market extends Component {
+  constructor() {
+    super()
+    this.state = {
+      seedModalVisibile: false
+    }
+    this.toggleSeedModal = this.toggleSeedModal.bind(this)
+  }
+
+  toggleSeedModal() {
+    this.setState({ seedModalVisibile: !this.state.seedModalVisibile })
+  }
+
+  render() {
+    return (
+      <View>
+        <ScrollView>
+          <Card title="SELL NOW" image={require("./res/sell_crops.png")}>
+            <Button
+              // icon={<Icon name="code" color="#ffffff" />}
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0
+              }}
+              title="SELECT CROPS TO SELL"
+            />
+          </Card>
+          <Card title="SEEDS" image={require("./res/seed_pack.png")}>
+            <Button
+              // icon={<Icon name="code" color="#ffffff" />}
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0
+              }}
+              title="BUY NOW"
+              onPress={this.toggleSeedModal}
+            />
+          </Card>
+          {this.state.seedModalVisibile ? <SeedsModal parentToggle={this.toggleSeedModal}/> : null}
+          <Card title="TOOLS" image={require("./res/hoe.png")}>
+            <Button
+              // icon={<Icon name="code" color="#ffffff" />}
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0
+              }}
+              title="BUY NOW"
+            />
+          </Card>
+          <Card title="PROTECTION" image={require("./res/umbrella.png")}>
+            <Button
+              // icon={<Icon name="code" color="#ffffff" />}
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0
+              }}
+              title="BUY NOW"
+            />
+          </Card>
+        </ScrollView>
+      </View>
+    )
+  }
+}
 
 // var styles = StyleSheet.create({
 //   img: {
@@ -65,4 +83,4 @@ const Market = () => {
 //   }
 // });
 
-module.exports = Market;
+module.exports = Market
--- ViroSample/js/SeedsModal.js
+++ ViroSample/js/SeedsModal.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react"
+import { View, Text, Modal, TouchableHighlight } from "react-native"
+import { Market } from "./Market"
 
-export default function SeedsModal() {
+export default class SeedsModal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: true
+    }
+    this.viewToggle = this.viewToggle.bind(this)
+  }
+
+  viewToggle() {
+    this.props.parentToggle()
+    this.setState({visible: false})
+  }
+
+  render() {
   return (
-    <div>
+    <View>
+      <Modal
+        animationType={"slide"}
+        transparent={false}
+        visible={this.state.visible}
+      >
+        <View>
+          <Text>Seed Modal Is Open!</Text>
 
-    </div>
+          <TouchableHighlight onPress={this.viewToggle}>
+            <Text>Close Modal</Text>
+          </TouchableHighlight>
+        </View>
+      </Modal>
+    </View>
   )
+  }
 }
-
-
